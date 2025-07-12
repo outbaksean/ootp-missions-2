f0b5ac3228278073504abb7e37d83b02938e5448
Sean <seanehrig@gmail.com>
2025-07-12 18:07:17 -0400

    Move calculateMissionDetails out of initialize for missions with type points and missions

--- ootp-missions/src/stores/useMissionStore.ts
+++ ootp-missions/src/stores/useMissionStore.ts
@@ -12,12 +12,116 @@ export const useMissionStore = defineStore('mission', () => {
   const selectedPriceType = ref<PriceType>({ sellPrice: false })
   const loading = ref(true)
 
+  async function calculateMissionDetails(missionId: number) {
+    const userCards = await db.userCards.toArray()
+    const shopCards = await db.shopCards.toArray()
+    const userMission = userMissions.value.find((m) => m.id === missionId)
+    if (!userMission) {
+      return
+    }
+    loading.value = true
+    const mission = userMission.rawMission
+    if (mission.type === 'points') {
+      const remainingPrice = MissionHelper.calculateTotalPriceOfNonOwnedCards(
+        mission,
+        userCards,
+        shopCards,
+        selectedPriceType.value.sellPrice,
+      )
+      const completed = MissionHelper.isMissionComplete(mission, userCards)
+      const missionCards = mission.cards
+        .map((card) => {
+          const shopCard = shopCards.find((shopCard) => shopCard.cardId == card.cardId)
+
+          if (!shopCard || shopCard.cardId === undefined) {
+            return null
+          }
+
+          const userCard = userCards.find((userCard) => userCard.cardId == card.cardId)
+
+          const price = selectedPriceType.value.sellPrice
+            ? shopCard.sellOrderLow || shopCard.lastPrice
+            : shopCard.lastPrice
+
+          const highlighted =
+            remainingPrice.totalPrice > 0 &&
+            remainingPrice.includedCards.some((c) => c.cardId == card.cardId)
+
+          return {
+            cardId: shopCard.cardId,
+            title: shopCard.cardTitle,
+            owned: !!userCard,
+            locked: userCard?.lock || false,
+            price,
+            highlighted: highlighted,
+            points: card.points || 0,
+          }
+        })
+        .filter((card) => card !== null)
+        .sort((a, b) => {
+          if (a.owned !== b.owned) return a.owned ? 1 : -1
+          if (a.locked !== b.locked) return a.locked ? 1 : -1
+          return a.price - b.price
+        })
+      let progressText = ''
+      const ownedPoints = userCards.reduce((total, userCard) => {
+        const card = mission?.cards.find((missionCard) => missionCard.cardId == userCard.cardId)
+        return total + (card?.points || 0)
+      }, 0)
+
+      const remainingCount = (mission.requiredCount ?? 0) - ownedPoints
+      if (remainingCount <= 0) {
+        progressText = `Completed with ${ownedPoints} points out of ${mission.requiredCount} of any ${mission.totalPoints} total`
+      } else {
+        progressText = `${ownedPoints} points out of ${mission.requiredCount} of any ${mission.totalPoints} total (${remainingCount} remaining)`
+      }
+
+      userMission.progressText = progressText
+      userMission.completed = completed
+      userMission.missionCards = missionCards
+      userMission.remainingPrice = remainingPrice.totalPrice
+    }
+    if (mission.type === 'missions') {
+      calculateMissionDetails(missionId)
+      const subMissions = userMissions.value.filter(
+        (userMission) =>
+          mission.missionIds && mission.missionIds.some((id) => id == userMission.rawMission.id),
+      )
+      const completedCount = subMissions.filter((m) => m.completed).length
+
+      // take the sum of the x lowest remaining prices of the subMissions where x is requiredCount minus completedCount
+      const remainingCount = mission.requiredCount - completedCount
+      const lowestRemainingPrices = subMissions
+        .filter((m) => !m.completed)
+        .filter((m) => m.remainingPrice > 0)
+        .map((m) => m.remainingPrice)
+        .sort((a, b) => a - b)
+        .slice(0, remainingCount)
+      const totalRemainingPrice = lowestRemainingPrices.reduce((sum, price) => sum + price, 0)
+
+      userMission.progressText = `${completedCount} out of ${mission.requiredCount} missions completed`
+      userMission.remainingPrice = totalRemainingPrice
+      userMission.completed = completedCount >= mission.requiredCount
+    }
+    loading.value = false
+  }
+
   async function initialize() {
     loading.value = true
     const shopCards = await db.shopCards.toArray()
     const userCards = await db.userCards.toArray()
 
     userMissions.value = rawMissions.map((mission) => {
+      if (mission.type == 'missions' || mission.type == 'points') {
+        return {
+          id: mission.id,
+          rawMission: mission,
+          progressText: 'Not Calculated',
+          completed: false,
+          missionCards: [],
+          remainingPrice: 0,
+        }
+      }
       const remainingPrice = MissionHelper.calculateTotalPriceOfNonOwnedCards(
         mission,
         userCards,
@@ -60,26 +164,11 @@ export const useMissionStore = defineStore('mission', () => {
           return a.price - b.price
         })
 
-      let progressText = ''
-      if (mission.type === 'points') {
-        const ownedPoints = userCards.reduce((total, userCard) => {
-          const card = mission?.cards.find((missionCard) => missionCard.cardId == userCard.cardId)
-          return total + (card?.points || 0)
-        }, 0)
+      const ownedCount = userCards.filter((userCard) =>
+        mission?.cards.some((card) => card.cardId == userCard.cardId),
+      ).length
 
-        const remainingCount = (mission.requiredCount ?? 0) - ownedPoints
-        if (remainingCount <= 0) {
-          progressText = `Completed with ${ownedPoints} points out of ${mission.requiredCount} of any ${mission.totalPoints} total`
-        } else {
-          progressText = `${ownedPoints} points out of ${mission.requiredCount} of any ${mission.totalPoints} total (${remainingCount} remaining)`
-        }
-      } else {
-        const ownedCount = userCards.filter((userCard) =>
-          mission?.cards.some((card) => card.cardId == userCard.cardId),
-        ).length
-
-        progressText = `${ownedCount} out of any ${mission.requiredCount} of ${mission.totalPoints} total`
-      }
+      const progressText = `${ownedCount} out of any ${mission.requiredCount} of ${mission.totalPoints} total`
 
       loading.value = false
       return {
@@ -89,31 +178,6 @@ export const useMissionStore = defineStore('mission', () => {
         completed: completed,
         missionCards: missionCards,
         remainingPrice: remainingPrice.totalPrice,
-      }
-    })
-    userMissions.value.forEach((mission) => {
-      if (mission.rawMission.type === 'missions') {
-        // Count the number of userMissions that are completed and have missionIds in this mission
-        const subMissions = userMissions.value.filter(
-          (userMission) =>
-            mission.rawMission.missionIds &&
-            mission.rawMission.missionIds.some((id) => id == userMission.rawMission.id),
-        )
-        const completedCount = subMissions.filter((m) => m.completed).length
-
-        // take the sum of the x lowest remaining prices of the subMissions where x is requiredCount minus completedCount
-        const remainingCount = mission.rawMission.requiredCount - completedCount
-        const lowestRemainingPrices = subMissions
-          .filter((m) => !m.completed)
-          .filter((m) => m.remainingPrice > 0)
-          .map((m) => m.remainingPrice)
-          .sort((a, b) => a - b)
-          .slice(0, remainingCount)
-        const totalRemainingPrice = lowestRemainingPrices.reduce((sum, price) => sum + price, 0)
-
-        mission.progressText = `${completedCount} out of ${mission.rawMission.requiredCount} missions completed`
-        mission.remainingPrice = totalRemainingPrice
-        mission.completed = completedCount >= mission.rawMission.requiredCount
       }
     })
   }
